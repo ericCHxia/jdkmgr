f3c2c34c70112d7f0264cc75a54706bda1b71632
EricXia <xch@raxch.cn>
2022-02-01 11:07:40 +0800

    ✨ feat(jdkmgr): add maven support

--- src/jdkmgr.py
+++ src/jdkmgr.py
@@ -3,6 +3,7 @@ import json
 import platform
 import argparse
 from jdk import JDKManager
+from maven import MavenManager
 
 os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 
@@ -12,20 +13,35 @@ if __name__ == '__main__':
         with open("config.json") as f:
             config = json.load(f)
     manager = JDKManager(config.get("jdk", None))
+    maven_manager = MavenManager(config.get("maven", None))
 
     parser = argparse.ArgumentParser(description="JDK Manager")
     subparsers = parser.add_subparsers()
 
-    parser_ls = subparsers.add_parser('ls')
+    subparsers_java = subparsers.add_parser("java", help="JDK").add_subparsers()
+    subparsers_maven = subparsers.add_parser("mvn", help="Maven").add_subparsers()
+
+    parser_ls = subparsers_java.add_parser('ls')
     parser_ls.set_defaults(func=manager.list)
 
-    parser_install = subparsers.add_parser('install')
+    parser_install = subparsers_java.add_parser('install')
     parser_install.add_argument('name', help="Name of the JDK")
     parser_install.set_defaults(func=manager.install)
 
-    parser_use = subparsers.add_parser('use')
-    parser_use.add_argument('jdk', type=str, help="JDK hash or JDK dir name")
+    parser_use = subparsers_java.add_parser('use')
+    parser_use.add_argument('name', type=str, help="JDK hash or JDK dir name")
     parser_use.set_defaults(func=manager.use)
+    
+    maven_parser_ls = subparsers_maven.add_parser('ls')
+    maven_parser_ls.set_defaults(func=maven_manager.list)
+
+    maven_parser_install = subparsers_maven.add_parser('install')
+    maven_parser_install.add_argument('name', help="Name of the Maven")
+    maven_parser_install.set_defaults(func=maven_manager.install)
+
+    maven_parser_use = subparsers_maven.add_parser('use')
+    maven_parser_use.add_argument('name', type=str, help="Maven hash or Maven dir name")
+    maven_parser_use.set_defaults(func=maven_manager.use)
 
     args = parser.parse_args()
     if args.__contains__("func"):
@@ -35,5 +51,6 @@ if __name__ == '__main__':
         exit(1)
 
     config["jdk"] = manager.jdk_path
+    config["maven"] = maven_manager.maven_path
     with open("config.json", "w") as f:
         json.dump(config, f)
---
+++ src/maven.py
@@ -0,0 +1,179 @@
+import os
+import platform
+import json
+from utils import download, extract, create_link, get_avaliable_arches
+
+
+class MavenManager:
+
+    def __init__(self, maven_path=None) -> None:
+        """Initialize MavenManager with maven_path
+
+        Args:
+            maven_path (str, optional): path to the maven.
+
+        Raises:
+            FileNotFoundError: if source/maven.json not found
+
+        Examples:
+            >>> maven = MavenManager()
+            >>> maven = MavenManager("maven_3.6.3")
+        """
+        self.maven_path = maven_path
+
+        # load Maven sources
+        if os.path.exists("source/maven.json"):
+            with open("source/maven.json") as f:
+                self.maven_sources = json.load(f)
+        else:
+            raise FileNotFoundError("No such file: source/maven.json")
+
+        self.indstalled = {}
+        self.indstalled_hash = set()
+        if not os.path.exists("install"):
+            os.makedirs("install")
+
+        for i in os.listdir("install"):
+            if i.startswith("maven_"):
+                if os.path.exists(os.path.join("install", i, "release.json")) and (os.path.exists(os.path.join("install", i, "bin", "mvn")) or os.path.exists(os.path.join("install", i, "bin", "mvn.cmd"))):
+                    with open(os.path.join("install", i, "release.json")) as f:
+                        self.indstalled[i] = json.load(f)
+                        self.indstalled_hash.add(
+                            self.get_hash(self.indstalled[i]))
+
+    @staticmethod
+    def get_hash(maven_source: dict) -> str:
+        """get hash of the JDK
+
+        Examples:
+        >>> maven = MavenManager()
+        >>> maven.get_hash({"url": "https://archive.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip", "md5": "c87c4caae590d48f90e02fc7e254dabe", "version": "3.6.3"})
+
+        Args:
+            maven_source (dict): maven source
+
+        Returns:
+            str: hash of the JDK
+        """
+        return maven_source["sha512"] if "sha512" in maven_source else maven_source["sha256"] if "sha256" in maven_source else maven_source["sha1"] if "sha1" in maven_source else maven_source["md5"] if "md5" in maven_source else None
+
+    @staticmethod
+    def get_hashs(maven_source: dict) -> list:
+        """get hash of the JDK
+
+        Examples:
+        >>> maven = MavenManager()
+        >>> maven.get_hashs({"url": "https://archive.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip", "md5": "c87c4caae590d48f90e02fc7e254dabe", "version": "3.6.3"})
+
+        Args:
+            maven_source (dict): maven source
+
+        Returns:
+            dict: hash of the JDK
+        """
+        hashs = {}
+        for i in ["sha512", "sha256", "sha1", "md5"]:
+            if i in maven_source:
+                hashs[i] = maven_source[i]
+        return hashs
+
+    @staticmethod
+    def generate_name(maven_source: dict) -> str:
+        """Generate a name for the Maven source with the following format: maven_<version>
+
+        Args:
+            maven_source (dict): maven source
+
+        Returns:
+            str: name of the JDK
+        """
+        return f"maven_{maven_source['version']}"
+
+    def install(self, name: str, **kargs) -> bool:
+        """install Maven
+
+        Examples:
+        >>> maven = MavenManager()
+        >>> maven.install("maven_3.6.3")
+
+        Args:
+            name (str): name of the Maven
+
+        Returns:
+            bool: True if installed successfully else False
+        """
+        for i in self.maven_sources:
+            if self.generate_name(i) == name:
+
+                # check if already installed
+                if self.get_hash(i) in self.indstalled_hash:
+                    print(f"Maven {self.generate_name(i)} already installed")
+                    return False
+
+                file_name = os.path.split(i["url"])[1]
+                file_path = os.path.join("cache", file_name)
+                os.makedirs(os.path.join("cache"), exist_ok=True)
+                download(i["url"], file_path, **self.get_hashs(i))
+                extract(file_path, "install", self.generate_name(i))
+
+                # create release.json
+                with open(os.path.join("install", self.generate_name(i), "release.json"), "w") as f:
+                    json.dump(i, f)
+                self.indstalled[self.generate_name(i)] = i
+                self.indstalled_hash.add(self.get_hash(i))
+
+                return True
+        
+        print(f"No such Maven {name}")
+        return False
+    
+    def use(self, name: str, **kargs) -> bool:
+        """use Maven
+
+        Examples:
+        >>> maven = MavenManager()
+        >>> maven.use("maven_3.6.3")
+
+        Args:
+            name (str): name of the Maven
+
+        Returns:
+            bool: True if used successfully else False
+        """
+        if name in self.indstalled:
+            if os.path.exists("maven"):
+                os.remove("maven")
+            os.symlink(os.path.join("install", name), "maven")
+            print(f"Maven {name} is used")
+            return True
+        else:
+            print(f"No such Maven {name}")
+            return False
+    
+    def list(self, **kargs) -> None:
+        """list all installed Maven
+
+        Examples:
+        >>> maven = MavenManager()
+        >>> maven.list()
+
+        ToDo:
+            add more info
+            print sorted by version
+        """
+        print("Installed Maven:")
+        for i in self.indstalled:
+            print(f"  {i}",end="")
+            if self.indstalled[i]["version"] == self.maven_sources[0]["version"]:
+                print(" *")
+            else:
+                print()
+        
+        print("\nAvailable Maven:")
+        available_maven = set()
+        for i in self.maven_sources:
+            if self.generate_name(i) not in self.indstalled:
+                available_maven.add(self.generate_name(i))
+        for i in available_maven:
+            print(f"  {i}")
+
